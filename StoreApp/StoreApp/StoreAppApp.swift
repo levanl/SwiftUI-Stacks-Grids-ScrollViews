bc355328b3db2805dcc68407b2f9e2a21b5aced3
Levan Loladze <levan.loladzew@gmail.com>
2023-12-17 14:46:06 +0400

    first commit

---
+++ StoreApp/StoreApp/StoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppApp.swift
+//  StoreApp
+//
+//  Created by Levan Loladze on 17.12.23.
+//
+
+import SwiftUI
+
+@main
+struct StoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
